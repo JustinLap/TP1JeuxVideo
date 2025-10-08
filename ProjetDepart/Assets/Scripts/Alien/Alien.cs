ead2c5e7e5e7d625a003a087e2bd0776d5f974c0
JustinLap <justinlaprise02@gmail.com>
2025-10-08 09:25:39 -0400

    Modifications Justin après Cours 2

---
+++ ProjetDepart/Assets/Scripts/Alien/Alien.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Alien : MonoBehaviour
+{
+    [SerializeField] private int moveSpeed = 10;
+    [SerializeField] private int rotationSpeed = 120;
+    // [SerializeField] private int damagePlayer = 10;
+
+    private NavMeshAgent agent;
+    private SpaceMarine spaceMarine; 
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        if (agent != null)
+        {
+            agent.speed = moveSpeed;
+            agent.angularSpeed = rotationSpeed;
+        }
+
+        spaceMarine = FindAnyObjectByType<SpaceMarine>();
+    }
+
+    void Update()
+    {
+        if (spaceMarine != null && agent != null)
+        {
+            agent.SetDestination(spaceMarine.transform.position);
+        }
+    }
+
+    //Dommages (à compléter)
+    void OnCollisionEnter(Collision other)
+    {
+        var marine = other.gameObject.GetComponent<SpaceMarine>();
+        if (marine != null)
+        {
+
+        }
+    }
+}
---
+++ ProjetDepart/Assets/Scripts/Alien/AlienSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class AlienSpawner : MonoBehaviour
+{
+    [SerializeField] private Portal[] spawnPoints;
+    [SerializeField] private float spawnDelay = 2f;
+    [SerializeField] private int alienLimit = 20;
+
+    private int alienCount = 0;
+    private ObjectPool alienPool;
+
+    private void Awake()
+    {
+
+        alienPool = Finder.ObjectPools.Alien;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (alienCount < alienLimit)
+        {
+            int randomIndex = Random.Range(0, spawnPoints.Length);
+            Portal chosenPortal = spawnPoints[randomIndex];
+
+            GameObject alien = alienPool.Get();
+            if (alien != null)
+            {
+                alien.transform.SetPositionAndRotation(chosenPortal.transform.position,chosenPortal.transform.rotation);
+
+                alienCount++;
+            }
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
